7337758889118c47805ae75a1a566a87da724604
Salman Jaher <smj190003@utdallas.edu>
2021-11-02 13:27:23 -0500

    2nd

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import Activities from './Activities';
 import Parks from './Parks';
+import WebStream from './WebStream';
 
 const url =
   'https://developer.nps.gov/api/v1/parks?limit=496&api_key=wbakT9pi2jO0k5wzrWTRx9F3FbElu7z0alH59mqz';
 
 function Body() {
-  const [parkData, setParkData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [parkList, setParkList] = useState([]);
   const [choices, setChoices] = useState([]);
+  const [watchList, setWatchList] = useState(false);
+  const [watchListData, setWatchListData] = useState([]);
 
   const addChoices = (name) => {
     if (!choices.includes(name)) {
       setChoices([...choices, name]);
     }
+  };
+
+  const watchHandle = () => {
+    setWatchList(!watchList);
   };
 
   const filterActivity = (id) => {
@@ -31,7 +37,6 @@ function Body() {
   const fetchData = async () => {
     const data = await fetch(url);
     const info = await data.json();
-    setParkData(info);
     setParkList(info.data);
     setIsLoading(false);
   };
@@ -59,9 +64,17 @@ function Body() {
       </>
     );
   }
+  if (watchList) {
+    return (
+      <>
+        <WebStream watchHandle={watchHandle} watchListData={watchListData} />
+      </>
+    );
+  }
   if (!isLoading) {
     return (
       <div>
+        <button onClick={watchHandle}>To Watchlist</button>
         <Activities
           key={parkList.id}
           addChoices={addChoices}
