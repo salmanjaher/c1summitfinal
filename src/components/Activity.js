f38e6ff3d0db76de8712ef6d529328317e05dc76
Salman Jaher <smj190003@utdallas.edu>
2021-11-09 12:10:01 -0600

    update

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Component that displays activities as different buttons based on paramateres passed in.
-const Activity = ({ id, name, filterActivity, addChoices }) => {
+function Activity({ id, name, filterActivity, addChoices }) {
   return (
     <button
       class='bg-yellow-700 hover:bg-yellow-800 font-thin text-white py-1 px-2 '
@@ -13,6 +13,6 @@ const Activity = ({ id, name, filterActivity, addChoices }) => {
       {name}
     </button>
   );
-};
+}
 
 export default Activity;
